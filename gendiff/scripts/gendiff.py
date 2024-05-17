2e2c62eb50876706228b047d8e9bdd7113d4c7ae
qffo <075viktor@mail.ru>
2024-05-17 12:20:03 +0300

    change return on print

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -9,7 +9,7 @@ def main():
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     args = parser.parse_args()
-    return args
+    print(args.first_file, args.second_file)
 
 
 if __name__ == '__main__':
