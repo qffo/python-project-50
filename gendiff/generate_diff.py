172752c10eff77328e1cd894be2eae6d0edfb182
qffo <075viktor@mail.ru>
2024-05-18 23:10:17 +0300

    refak gendif funk

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,10 +1,30 @@
 import json
 
 
-def generate_diff(file_path1, file_path2):
+def generate_diff(file_path1, file_path2, format=json):
     f1 = json.load(open(file_path1))
     f2 = json.load(open(file_path2))
-    return f1, f2
+    result = {}
+    foo = ''
+    start = '{'
+    end = '}'
+    # print(f1, f2)
+    for k, v in f1.items():
+        if f1.get(k) == f2.get(k):
+            result.setdefault(k, v)
+        if f1.get(k) != f2.get(k):
+            result.setdefault(f'- {k}', v)
+    for k, v in f2.items():
+        if f2.get(k) != f1.get(k):
+            result.setdefault(f'+ {k}', v)
+    for k, v in result.items():
+        if '-' not in k and '+' not in k:
+            # print(k, v)
+            foo += f"  {str(k)}: {str(v).lower()}\n"
+        else:
+            foo += f"{str(k)}: {str(v).lower()}\n"
+    return f"{start}\n{foo}{end}"
 
 
-print(generate_diff('files_for_test/file1.json', 'files_for_test/file2.json'))
+# return '{\n- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true\n}'
+# print(generate_diff('files_for_test/file1.json', 'files_for_test/file2.json'))
