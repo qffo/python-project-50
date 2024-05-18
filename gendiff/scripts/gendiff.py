d4a4103fbf2c21c6fd44515800c9e4dd84727743
qffo <075viktor@mail.ru>
2024-05-18 08:53:18 +0300

    modif -f format

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -8,8 +8,7 @@ def main():
         description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument(
-        "-f", "--format", metavar="FORMAT", help="set format of output")
+    parser.add_argument("-f", "--format", help="set format of output")
     args = parser.parse_args()
     print(args.first_file, args.second_file, args.format)
 
