f0d1f7699be396c4a6facbf6adaa355b797efc32
qffo <075viktor@mail.ru>
2024-05-21 22:55:25 +0300

    add pyyaml in projet

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,26 +1,56 @@
 import json
+import yaml
 
 
 def generate_diff(file_path1, file_path2, format=json):  # noqa: C901
-    f1 = json.load(open(file_path1))
-    f2 = json.load(open(file_path2))
-    result = []
-    foo = ''
-    for k, v in f1.items():
-        if f1.get(k) == f2.get(k):
-            result.append(('', k, v))
-        if f1.get(k) != f2.get(k):
-            result.append(('-', k, v))
-    for k, v in f2.items():
-        if f2.get(k) != f1.get(k):
-            result.append(('+', k, v))
-    result = sorted(result, key=lambda x: x[1])
-    for i, k, v in result:
-        if i != '-' and i != '+':
-            foo += f'   {str(i)} {str(k)}: {(str(v)).lower()}\n'
-        else:
-            foo += f'  {str(i)} {str(k)}: {(str(v)).lower()}\n'
-    return f"{'{'}\n{foo}{'}'}"
+
+    def diff_yml(file_path1, file_path2):
+        f1 = yaml.load(open(file_path1), Loader=yaml.FullLoader) or {}
+        f2 = yaml.load(open(file_path2), Loader=yaml.FullLoader) or {}
+        result = []
+        foo = ''
+        for k, v in f1.items():
+            if f1.get(k) == f2.get(k):
+                result.append(('', k, v))
+            if f1.get(k) != f2.get(k):
+                result.append(('-', k, v))
+        for k, v in f2.items():
+            if f2.get(k) != f1.get(k):
+                result.append(('+', k, v))
+        result = sorted(result, key=lambda x: x[1])
+        for i, k, v in result:
+            if i != '-' and i != '+':
+                foo += f'   {str(i)} {str(k)}: {(str(v)).lower()}\n'
+            else:
+                foo += f'  {str(i)} {str(k)}: {(str(v)).lower()}\n'
+        return f"{'{'}\n{foo}{'}'}"
+
+    def diff_json(file_path1, file_path2):
+        f1 = json.load(open(file_path1))
+        f2 = json.load(open(file_path2))
+        result = []
+        foo = ''
+        for k, v in f1.items():
+            if f1.get(k) == f2.get(k):
+                result.append(('', k, v))
+            if f1.get(k) != f2.get(k):
+                result.append(('-', k, v))
+        for k, v in f2.items():
+            if f2.get(k) != f1.get(k):
+                result.append(('+', k, v))
+        result = sorted(result, key=lambda x: x[1])
+        for i, k, v in result:
+            if i != '-' and i != '+':
+                foo += f'   {str(i)} {str(k)}: {(str(v)).lower()}\n'
+            else:
+                foo += f'  {str(i)} {str(k)}: {(str(v)).lower()}\n'
+        return f"{'{'}\n{foo}{'}'}"
+
+    if file_path1.endswith('json'):
+        return diff_json(file_path1, file_path2)
+    if file_path1.endswith('yml'):
+        return diff_yml(file_path1, file_path2)
 
 
-# print(generate_diff('files_for_test/file1.json', 'files_for_test/file2.json'))
+# print(generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json'))
+# print(generate_diff('tests/fixtures/file1.yml', 'tests/fixtures/file2.yaml'))
--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -4,3 +4,8 @@ from gendiff.generate_diff import generate_diff
 def test_generate_diff():
     assert generate_diff('tests/fixtures/file1.json',
                          'tests/fixtures/file2.json') == '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}'
+
+
+def test_generate_diff_yml():
+    assert generate_diff('tests/fixtures/file1.yml',
+                         'tests/fixtures/file2.yaml') == '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}'
