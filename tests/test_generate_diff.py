ec2a581536db68c401518801c399172d5de382b1
qffo <075viktor@mail.ru>
2024-06-16 17:00:23 +0300

    add test for json

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -64,3 +64,103 @@ Property 'group1.baz' was updated. From 'bas' to 'bars'
 Property 'group1.nest' was updated. From [complex value] to 'str'
 Property 'group2' was removed
 Property 'group3' was added with value: [complex value]"""
+
+assert generate_diff('tests/fixtures/file3.json',
+                     'tests/fixtures/file4.json', 'json') == """{
+    "common": {
+        "type": "nested",
+        "value": {
+            "follow": {
+                "type": "added",
+                "value": false
+            },
+            "setting1": {
+                "type": "unchanged",
+                "value": "Value 1"
+            },
+            "setting2": {
+                "type": "removed",
+                "value": 200
+            },
+            "setting3": {
+                "type": "changed",
+                "old": true,
+                "new": null
+            },
+            "setting4": {
+                "type": "added",
+                "value": "blah blah"
+            },
+            "setting5": {
+                "type": "added",
+                "value": {
+                    "key5": "value5"
+                }
+            },
+            "setting6": {
+                "type": "nested",
+                "value": {
+                    "doge": {
+                        "type": "nested",
+                        "value": {
+                            "wow": {
+                                "type": "changed",
+                                "old": "",
+                                "new": "so much"
+                            }
+                        }
+                    },
+                    "key": {
+                        "type": "unchanged",
+                        "value": "value"
+                    },
+                    "ops": {
+                        "type": "added",
+                        "value": "vops"
+                    }
+                }
+            }
+        }
+    },
+    "group1": {
+        "type": "nested",
+        "value": {
+            "baz": {
+                "type": "changed",
+                "old": "bas",
+                "new": "bars"
+            },
+            "foo": {
+                "type": "unchanged",
+                "value": "bar"
+            },
+            "nest": {
+                "type": "changed",
+                "old": {
+                    "key": "value"
+                },
+                "new": "str"
+            }
+        }
+    },
+    "group2": {
+        "type": "removed",
+        "value": {
+            "abc": 12345,
+            "deep": {
+                "id": 45
+            }
+        }
+    },
+    "group3": {
+        "type": "added",
+        "value": {
+            "deep": {
+                "id": {
+                    "number": 45
+                }
+            },
+            "fee": 100500
+        }
+    }
+}"""
