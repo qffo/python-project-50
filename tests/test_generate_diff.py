9487f581e4f2e8f28f65b8ea1c7dc8be9d0d49bf
qffo <075viktor@mail.ru>
2024-06-16 14:11:53 +0300

    add test for plain

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -50,3 +50,17 @@ def test_generate_diff():
         fee: 100500
     }
 }"""
+
+
+assert generate_diff('tests/fixtures/file3.json',
+                     'tests/fixtures/file4.json', 'plain') == """Property 'common.follow' was added with value: false
+Property 'common.setting2' was removed
+Property 'common.setting3' was updated. From true to null
+Property 'common.setting4' was added with value: 'blah blah'
+Property 'common.setting5' was added with value: [complex value]
+Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
+Property 'common.setting6.ops' was added with value: 'vops'
+Property 'group1.baz' was updated. From 'bas' to 'bars'
+Property 'group1.nest' was updated. From [complex value] to 'str'
+Property 'group2' was removed
+Property 'group3' was added with value: [complex value]"""
