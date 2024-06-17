d148bbb5c70f5e960df1af03d9adb23a9d1fd0c6
qffo <075viktor@mail.ru>
2024-06-17 19:13:17 +0300

    refact tests

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,166 +1,25 @@
+import pytest
 from gendiff.generate_diff import generate_diff
 
+yml_file1 = 'tests/fixtures/file1.yml'
+yaml_file2 = 'tests/fixtures/file2.yaml'
 
-def test_generate_diff():
-    assert generate_diff('tests/fixtures/file1.json',
-                         'tests/fixtures/file2.json') == '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}'
+file3 = 'tests/fixtures/file3.json'
+file4 = 'tests/fixtures/file4.json'
 
-    assert generate_diff('tests/fixtures/file3.yaml',
-                         'tests/fixtures/file4.json') == """{
-    common: {
-      + follow: false
-        setting1: Value 1
-      - setting2: 200
-      - setting3: true
-      + setting3: null
-      + setting4: blah blah
-      + setting5: {
-            key5: value5
-        }
-        setting6: {
-            doge: {
-              - wow: 
-              + wow: so much
-            }
-            key: value
-          + ops: vops
-        }
-    }
-    group1: {
-      - baz: bas
-      + baz: bars
-        foo: bar
-      - nest: {
-            key: value
-        }
-      + nest: str
-    }
-  - group2: {
-        abc: 12345
-        deep: {
-            id: 45
-        }
-    }
-  + group3: {
-        deep: {
-            id: {
-                number: 45
-            }
-        }
-        fee: 100500
-    }
-}"""
+linear = 'tests/fixtures/linear'
+stylish = 'tests/fixtures/nested_stylish'
+plain = 'tests/fixtures/nested_plain'
 
 
-assert generate_diff('tests/fixtures/file3.json',
-                     'tests/fixtures/file4.json', 'plain') == """Property 'common.follow' was added with value: false
-Property 'common.setting2' was removed
-Property 'common.setting3' was updated. From true to null
-Property 'common.setting4' was added with value: 'blah blah'
-Property 'common.setting5' was added with value: [complex value]
-Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
-Property 'common.setting6.ops' was added with value: 'vops'
-Property 'group1.baz' was updated. From 'bas' to 'bars'
-Property 'group1.nest' was updated. From [complex value] to 'str'
-Property 'group2' was removed
-Property 'group3' was added with value: [complex value]"""
-
-assert generate_diff('tests/fixtures/file3.json',
-                     'tests/fixtures/file4.json', 'json') == """{
-    "common": {
-        "type": "nested",
-        "value": {
-            "follow": {
-                "type": "added",
-                "value": false
-            },
-            "setting1": {
-                "type": "unchanged",
-                "value": "Value 1"
-            },
-            "setting2": {
-                "type": "removed",
-                "value": 200
-            },
-            "setting3": {
-                "type": "changed",
-                "old": true,
-                "new": null
-            },
-            "setting4": {
-                "type": "added",
-                "value": "blah blah"
-            },
-            "setting5": {
-                "type": "added",
-                "value": {
-                    "key5": "value5"
-                }
-            },
-            "setting6": {
-                "type": "nested",
-                "value": {
-                    "doge": {
-                        "type": "nested",
-                        "value": {
-                            "wow": {
-                                "type": "changed",
-                                "old": "",
-                                "new": "so much"
-                            }
-                        }
-                    },
-                    "key": {
-                        "type": "unchanged",
-                        "value": "value"
-                    },
-                    "ops": {
-                        "type": "added",
-                        "value": "vops"
-                    }
-                }
-            }
-        }
-    },
-    "group1": {
-        "type": "nested",
-        "value": {
-            "baz": {
-                "type": "changed",
-                "old": "bas",
-                "new": "bars"
-            },
-            "foo": {
-                "type": "unchanged",
-                "value": "bar"
-            },
-            "nest": {
-                "type": "changed",
-                "old": {
-                    "key": "value"
-                },
-                "new": "str"
-            }
-        }
-    },
-    "group2": {
-        "type": "removed",
-        "value": {
-            "abc": 12345,
-            "deep": {
-                "id": 45
-            }
-        }
-    },
-    "group3": {
-        "type": "added",
-        "value": {
-            "deep": {
-                "id": {
-                    "number": 45
-                }
-            },
-            "fee": 100500
-        }
-    }
-}"""
+@pytest.mark.parametrize(
+    'path1, path2, format_name, expected',
+    [
+        (yml_file1, yaml_file2, 'stylish', linear),
+        (file3, file4, 'stylish', stylish),
+        (file3, file4, 'plain', plain),
+    ]
+)
+def test_generate_diff(path1, path2, format_name, expected):
+    with open(expected) as expectation:
+        assert generate_diff(path1, path2, format_name) == expectation.read()
