f7f52c40e4e4a2fe1639c74c26dabeca720043cd
qffo <075viktor@mail.ru>
2024-06-20 18:43:28 +0300

    add pars_arg function

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,14 +3,22 @@ from gendiff import generate_diff
 import argparse
 
 
-def main():
+def parsing_arguments():
     parser = argparse.ArgumentParser(
         description='Compares two configuration files and shows a difference.')
     parser.add_argument('file_path_1')
     parser.add_argument('file_path_2')
     parser.add_argument(
-        "-f", "--format", help="set format of output (default: 'stylish')")
-    args = parser.parse_args()
+        "-f",
+        "--format",
+        choices=['json', 'plain', 'stylish'],
+        default='stylish',
+        help="set format of output (default: 'stylish')")
+    return parser.parse_args()
+
+
+def main():
+    args = parsing_arguments()
     diff = generate_diff(args.file_path_1, args.file_path_2, args.format)
     print(diff)
 
