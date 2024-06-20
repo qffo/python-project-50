882fe32306cfd188e1fabaa1771c498e1fdae193
qffo <075viktor@mail.ru>
2024-06-20 18:42:45 +0300

    change [i] to get(i)

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -5,26 +5,22 @@ from .formated.plain_formated import format_plain
 from .formated.json_formated import format_json
 
 
-def gen_diff(data1: dict, data2: dict) -> dict:  # noqa: C901
+def gen_diff(data1: dict, data2: dict) -> dict:
     diff = {}
     keys = set(data1.keys() | set(data2.keys()))
 
     for i in keys:
-        if i not in data1.keys():
+        if isinstance(data1.get(i), dict) and isinstance(data2.get(i), dict):
+            diff[i] = {'type': 'nested',
+                       'value': gen_diff(data1[i], data2[i])}
+        elif i not in data1.keys():
             diff[i] = {'type': 'added', 'value': data2[i]}
-        if i not in data2.keys():
+        elif i not in data2.keys():
             diff[i] = {'type': 'removed', 'value': data1[i]}
-
-        if i in data1.keys() and i in data2.keys():
-            if isinstance(data1[i], dict) and isinstance(data2[i], dict):
-                diff[i] = {'type': 'nested',
-                           'value': gen_diff(data1[i], data2[i])}
-
-            elif data1[i] == data2[i]:
-                diff[i] = {'type': 'unchanged', 'value': data1[i]}
-
-            else:
-                diff[i] = {'type': 'changed', 'old': data1[i], 'new': data2[i]}
+        elif data1[i] == data2[i]:
+            diff[i] = {'type': 'unchanged', 'value': data1[i]}
+        else:
+            diff[i] = {'type': 'changed', 'old': data1[i], 'new': data2[i]}
 
     return OrderedDict(sorted(diff.items(), key=lambda k: k))
 
@@ -43,7 +39,3 @@ def generate_diff(file_path1: str, file_path2: str, format='stylish'):
             return format_plain(diff)
         case 'json':
             return format_json(diff)
-
-
-# print(generate_diff('tests/fixtures/file3.yaml', 'tests/fixtures/file4.json'))
-# print(generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json'))
